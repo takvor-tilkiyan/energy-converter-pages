c31fe6a56cf2c3c85237c05962013f95f0d96aae
takvor-tilkiyan <59090862+takvor-tilkiyan@users.noreply.github.com>
2019-12-20 16:30:29 +0200

    Add files via upload

---
+++ util/ConvertionDirection.js
@@ -0,0 +1,14 @@
+ConversionDirection = {
+	CALORIES_TO_JOULES : {
+		value : "calories_to_joules",
+		text : "Calories to Joules",
+		baseUnit : "kcal",
+		destinationUnit : "kJ"
+	},
+	JOULES_TO_CALORIES : {
+		value : "joules_to_calories",
+		text : "Joules to Calories",
+		baseUnit : "kJ",
+		destinationUnit : "kcal"
+	}
+}
